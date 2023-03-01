1e2551ca732cc3555ebb27b7b8531b66cabad48a
murphylee10 <murphylee2004@gmail.com>
2023-02-28 23:08:17 -0500

    update 2 - web

--- website/views.py
+++ website/views.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, redirect, render_template, request, flash, jsonify, url_for
 # from flask_login import login_required, current_user
 # from .models import Note
 # from . import db
 # from sqlalchemy.sql import func
 import json
+
+# courseText
+# quizQuestions
+# response
+
 
 # usually easier to keep the name the same as the file
 views = Blueprint('views', __name__)
@@ -13,11 +18,13 @@ views = Blueprint('views', __name__)
 # @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
+        query = request.form.get('query')
+        print(query)
 
-        if len(note) < 1:
-            flash('Note is too short!', category="error")
-        # else:
+        if len(query) < 1:
+            flash('Query is too short!', category="error")
+        else:
+            return redirect(url_for('views.query'))
             # new_note = Note(data=note, user_id=current_user.id)
             # print(current_user.id)
             # db.session.add(new_note)
@@ -25,6 +32,10 @@ def home():
             # flash("Note added!")
 
     return render_template("home.html")
+
+@views.route('/query', methods=['GET', 'POST'])
+def query():
+    return render_template('query.html', text="Hello World")
 
 # @views.route('/delete-note', methods=['POST'])
 # def delete_note():
