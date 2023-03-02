10f093b0fae59ee2b26b8ac3a42d2ad1f8f6eac9
murphylee10 <murphylee2004@gmail.com>
2023-03-02 12:37:16 -0500

    update 4 - web part basically done, just have to integrate openai parts

--- website/views.py
+++ website/views.py
@@ -11,6 +11,9 @@ import json
 # summary
 # images
 
+prompt = "default"
+courseParagraphs = []
+courseImages = []
 
 # usually easier to keep the name the same as the file
 views = Blueprint('views', __name__)
@@ -26,6 +29,13 @@ def home():
         if len(query) < 1:
             flash('Query is too short!', category="error")
         else:
+            global prompt, courseParagraphs, courseImages
+            prompt = query
+            for i in range(8):
+                courseParagraphs.append("Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sequi omnis facere, \
+                iure earum odit vel eum similique eaque sapiente alias, dicta praesentium placeat harum eveniet. \
+                Natus quasi saepe eum?")
+                courseImages.append(url_for('static', filename='images/Fantasy.jpeg'))
             return redirect(url_for('views.query'))
             # new_note = Note(data=note, user_id=current_user.id)
             # print(current_user.id)
@@ -37,7 +47,28 @@ def home():
 
 @views.route('/query', methods=['GET', 'POST'])
 def query():
-    return render_template('query.html', text="Hello World")
+    return render_template('query.html', prompt=prompt, courseParagraphs=courseParagraphs, courseImages=courseImages)
+
+@views.route('/generate-response', methods=['POST'])
+def generate_response():
+    prompt = json.loads(request.data)
+    promptText = prompt['text']
+    print(promptText)
+    return jsonify({"resp": "Hello User!"})
+
+@views.route('/generate-summary', methods=['POST'])
+def generate_summary():
+    # prompt = json.loads(request.data)
+    # promptText = prompt['text']
+    # print(promptText)
+    return jsonify({"resp": "Hello User!skjdfnkjsf;ksdjf;sdkjfs;dkjf;kld\nsdijfkdsjfkdsjf"})
+
+@views.route('/generate-quiz', methods=['POST'])
+def generate_quiz():
+    # prompt = json.loads(request.data)
+    # promptText = prompt['text']
+    # print(promptText)
+    return jsonify({"question": "What are the first 10 digits of pi?", "answer": "3.141592653", "reference": 3})
 
 # @views.route('/delete-note', methods=['POST'])
 # def delete_note():
