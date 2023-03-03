965c9af9ad66d1c6a60db1a2ed5aa735859a8145
Adibvafa Fallahpour <90617686+Adibvafa@users.noreply.github.com>
2023-03-02 22:16:07 -0500

    Update get_question.py

--- get_question.py
+++ get_question.py
@@ -66,29 +66,29 @@
 # print(qs)
 # print("\n")
 # print(ans)
-def get_question(arr):
-  import openai
-  import random
 
-  openai.api_key = "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"
+import openai
+import random
+openai.api_key = "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"
   
+def get_question(arr):
   package = {}
   package["reference"] = random.randint(1,3)
   paragraph = arr[package["reference"]]
   package["question"] = openai.Completion.create(
     model="text-davinci-003",
-    prompt=f"give a multiple choice or true/false question on this paragraph without giving the answer: {paragraph}",
+    prompt=f"Give a multiple choice or true/false question on this paragraph without giving the answer: {paragraph}",
     temperature=0.5,
-    max_tokens=2500,
+    max_tokens=500,
     frequency_penalty=0.0,
     presence_penalty=0.0
   )["choices"][0]["text"]
   question = package["question"]
   package["answer"] = openai.Completion.create(
     model="text-davinci-003",
-    prompt=f"give an answer to the question, followed by a comprehensive detailed explanation: {question}",
-    temperature=0.5,
-    max_tokens=2500,
+    prompt=f"Give an answer to the question, followed by a comprehensive detailed explanation: {question}",
+    temperature=0.2,
+    max_tokens=500,
     frequency_penalty=0.0,
     presence_penalty=0.0
   )["choices"][0]["text"]
