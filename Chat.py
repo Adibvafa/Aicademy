c611cb2405e3888eda396aaf0266b1e94bb39d7a
Adibvafa <adibvafafallahpour@gmail.com>
2023-03-02 21:22:25 -0500

    Added the chat function final

--- Chat.py
+++ Chat.py
@@ -7,21 +7,19 @@ Answers users question in the chat
 import openai
 openai.api_key = "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"
 
-def Answer_Question(user_input, user_question):
+def Answer_Question(user_question):
     """
-    Returns list of paragraphs, and list of images for index 0 and -3
-    :param user_input: Input of user to create the course
+    Answer to questions in the chatbox
     :param user_question: Question of user in the chat
     """
-    prompt = f"""User has asked {user_input} and you created a course based on it.
-    Now user has asked {user_question}. Provide accurate clear answer."""
+    prompt = f"""User has asked {user_question}. Provide accurate clear answer."""
 
     answer_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
-        temperature=0.1,
+        temperature=0.15,
         max_tokens=250,
-        top_p=0.95,
+        top_p=0.9,
     )
     answer = answer_response["choices"][0]["message"]["content"]
     return answer.strip()
